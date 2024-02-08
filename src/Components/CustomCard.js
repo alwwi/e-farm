0aa2361970a1c0a4de8e7dcaa1d773bff9b7e1b1
alwwi <alwialatas04@gmail.com>
2024-02-08 15:19:54 +0700

    add a dashboard page

---
+++ src/Components/CustomCard.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { Card, CardBody, CardText, Stack } from 'react-bootstrap'
+import Image from 'react-bootstrap/Image'
+import './Color.css';
+import './Font.css';
+
+export default class customCard extends Component {
+  render() {
+    return (
+      <div className='mt-5 d-flex mx-5 mb-4'>
+        <Stack direction="horizontal" gap={5}>
+
+      <>
+        <Card className='bg-card txt-poppins-medium' style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}>
+        <Image src={`${process.env.PUBLIC_URL}Image-Assets/cow_face.png`} width={95} height={95} alt="sapi" roundedCircle style={{position:'absolute', top:'-47px',left:'50%', transform:'translateX(-50%)'}}/>
+          <CardBody style={{paddingTop: '60px'}}>
+            <CardText className='mt-3 px-3'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </CardText>
+          </CardBody>
+        </Card>
+
+        <Card className='bg-card txt-poppins-medium' style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}>
+        <Image src={`${process.env.PUBLIC_URL}Image-Assets/chiken.png`} width={95} height={95} alt="ayam" roundedCircle style={{position:'absolute', top:'-47px',left:'50%', transform:'translateX(-50%)'}}/>
+          <CardBody style={{paddingTop: '60px'}}>
+            <CardText className='mt-3 px-3'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </CardText>
+          </CardBody>
+        </Card>
+
+        <Card className='bg-card txt-poppins-medium' style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}>
+        <Image src={`${process.env.PUBLIC_URL}Image-Assets/goat.png`} width={95} height={95} alt="kambing" roundedCircle style={{position:'absolute', top:'-47px',left:'50%', transform:'translateX(-50%)'}}/>
+          <CardBody style={{paddingTop: '60px'}}>
+            <CardText className='mt-3 px-3'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </CardText>
+          </CardBody>
+        </Card>
+
+        <Card className='bg-card txt-poppins-medium' style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}>
+        <Image src={`${process.env.PUBLIC_URL}Image-Assets/pig.png`} width={95} height={95} alt="babi" roundedCircle style={{position:'absolute', top:'-47px',left:'50%', transform:'translateX(-50%)'}}/>
+          <CardBody style={{paddingTop: '60px'}}>
+            <CardText className='mt-3 px-3'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </CardText>
+          </CardBody>
+        </Card>
+      </>
+        </Stack>
+      </div>
+    )
+  }
+}
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,35 +6,35 @@ import './Font.css';
 export default class Footer extends Component {
   render() {
     return (
-        <footer className="footer bg-footer py-5" style={{height:'320px'}}>
+        <footer className="footer bg-footer py-5" style={{height:'320px', color:'black'}}>
         <Container>
           <Row className="justify-content-md-center">
           <Col xs={12} md={2} className="text-start  mb-2  my-2">
-              <div className="text-wrapper txt-brand-footer" href="#home" >e-Farm</div>
+              <div className="text-wrapper txt-poppins-semiBold" href="#home" style={{fontSize:'22px'}}>e-Farm</div>
               <div className="txt-medium mt-3">Alamat</div>
             </Col>
             <Col xs={6} md={2} className="text-start  mb-3  my-3">
-              <div className="text-wrapper txt-footer" >e-Farm</div>
-              <div className="txt-medium mt-2">Home</div>
-              <div className="txt-medium">About</div>
+              <div className="text-wrapper txt-poppins-semiBold" style={{fontSize:'16px'}} >e-Farm</div>
+              <div className="txt-poppins-medium mt-2" style={{fontSize:'16px'}}>Home</div>
+              <div className="txt-poppins-medium" style={{fontSize:'16px'}}>About</div>
             </Col>
             <Col xs={6} md={2} className="text-start mb-3 my-3">
-              <div className="text-wrapper txt-footer">Contact</div>
-              <div className="txt-medium mt-2">Email</div>
-              <div className="txt-medium">Twitter</div>
-              <div className="txt-medium">Instagram</div>
-              <div className="txt-medium">Number Phone</div>
+              <div className="text-wrapper txt-poppins-semiBold" style={{fontSize:'16px'}}>Contact</div>
+              <div className="txt-poppins-medium mt-2" style={{fontSize:'16px'}}>Email</div>
+              <div className="txt-poppins-medium" style={{fontSize:'16px'}}>Twitter</div>
+              <div className="txt-poppins-medium" style={{fontSize:'16px'}}>Instagram</div>
+              <div className="txt-poppins-medium" style={{fontSize:'16px'}}>Number Phone</div>
             </Col>
             <Col xs={6} md={2} className="text-start mb-3 my-3">
-              <div className="text-wrapper txt-footer">Company</div>
-              <div className="txt-medium mt-2">Carrier</div>
-              <div className="txt-medium">Mitra</div>
+              <div className="text-wrapper txt-poppins-semiBold" style={{fontSize:'16px'}}>Company</div>
+              <div className="txt-poppins-medium mt-2" style={{fontSize:'16px'}}>Carrier</div>
+              <div className="txt-poppins-medium" style={{fontSize:'16px'}}>Mitra</div>
             </Col>
             <Col xs={6} md={2} className="text-start mb-3 my-3">
-              <div className="text-wrapper txt-footer">Help</div>
+              <div className="text-wrapper txt-poppins-semiBold" style={{fontSize:'16px'}}>Help</div>
             </Col>
             <Col xs={6} md={2} className="text-start mb-3 my-3">
-              <div className="text-wrapper txt-footer">Solution</div>
+              <div className="text-wrapper txt-poppins-semiBold" style={{fontSize:'16px'}}>Solution</div>
             </Col>
           </Row>
         </Container>
