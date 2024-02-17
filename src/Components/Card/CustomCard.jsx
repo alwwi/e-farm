2b13cb7449b5c29199a897309d9f09d903f0ba79
alwwi <alwialatas04@gmail.com>
2024-02-17 13:41:40 +0700

    complete page responsive

--- src/Components/Card/CustomCard.jsx
+++ src/Components/Card/CustomCard.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
 import { Card, CardBody, CardText, Stack } from 'react-bootstrap'
 import Image from 'react-bootstrap/Image'
-import './Color.css';
-import './Font.css';
+import '../Color.css';
+import '../Font.css';
+import './Card-Responsive.css'
 
 export default class customCard extends Component {
   render() {
     return (
-      <div className='mt-5 d-flex mx-5 mb-4'>
-        <Stack direction="horizontal" gap={5}>
+      <div className='mt-5 d-flex mx-5 mb-4 scroll-card'>
 
       <>
-        <Card className='bg-card txt-poppins-medium' style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}>
+        <Stack direction="horizontal" gap={5}>
+        <Card className='bg-card txt-poppins-medium' 
+        style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}
+        >
         <Image src={`${process.env.PUBLIC_URL}Image-Assets/cow_face.png`} width={95} height={95} alt="sapi" roundedCircle style={{position:'absolute', top:'-47px',left:'50%', transform:'translateX(-50%)'}}/>
           <CardBody style={{paddingTop: '60px'}}>
             <CardText className='mt-3 px-3'>
@@ -24,7 +27,9 @@ export default class customCard extends Component {
           </CardBody>
         </Card>
 
-        <Card className='bg-card txt-poppins-medium' style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}>
+        <Card className='bg-card txt-poppins-medium card' 
+        style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}
+        >
         <Image src={`${process.env.PUBLIC_URL}Image-Assets/chiken.png`} width={95} height={95} alt="ayam" roundedCircle style={{position:'absolute', top:'-47px',left:'50%', transform:'translateX(-50%)'}}/>
           <CardBody style={{paddingTop: '60px'}}>
             <CardText className='mt-3 px-3'>
@@ -37,7 +42,9 @@ export default class customCard extends Component {
           </CardBody>
         </Card>
 
-        <Card className='bg-card txt-poppins-medium' style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}>
+        <Card className='bg-card txt-poppins-medium card' 
+        style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}
+        >
         <Image src={`${process.env.PUBLIC_URL}Image-Assets/goat.png`} width={95} height={95} alt="kambing" roundedCircle style={{position:'absolute', top:'-47px',left:'50%', transform:'translateX(-50%)'}}/>
           <CardBody style={{paddingTop: '60px'}}>
             <CardText className='mt-3 px-3'>
@@ -50,7 +57,9 @@ export default class customCard extends Component {
           </CardBody>
         </Card>
 
-        <Card className='bg-card txt-poppins-medium' style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}>
+        <Card className='bg-card txt-poppins-medium card' 
+        style={{ width: '255px', height:'352px', fontSize:'12px',textAlign:'justify',position:'relative',overflow:'visible',borderRadius:'30px' }}
+        >
         <Image src={`${process.env.PUBLIC_URL}Image-Assets/pig.png`} width={95} height={95} alt="babi" roundedCircle style={{position:'absolute', top:'-47px',left:'50%', transform:'translateX(-50%)'}}/>
           <CardBody style={{paddingTop: '60px'}}>
             <CardText className='mt-3 px-3'>
@@ -62,8 +71,8 @@ export default class customCard extends Component {
             </CardText>
           </CardBody>
         </Card>
-      </>
         </Stack>
+      </>
       </div>
     )
   }
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import CustomNavbar from '../Components/CustomNavbar'
-import CustomCard from '../Components/CustomCard'
+import CustomNavbar from '../Components/Navbar/CustomNavbar'
+import CustomCard from '../Components/Card/CustomCard'
 import '../Components/Color.css';
 import '../Components/Font.css';
 import '../Components/Hover.css';
 import BtnFill from '../Components/BtnFill';
 import BtnOutline from '../Components/BtnOutline';
-// import ''
 import { Container, Row, Col, Image, Button, Stack } from 'react-bootstrap';
 import Footer from '../Components/Footer';
+import './Responsive-Dashboard.css';
 
 const Dashboard = () => {
     return (
@@ -16,11 +16,13 @@ const Dashboard = () => {
 
             <CustomNavbar />
 
-            <Container style={{ paddingLeft: '50px', paddingRight: '50px',paddingTop:'200px' }}>
-                <Row>
-                    <Col className='text-start'>
-                        <h1 className='txt-poppins-bold' style={{ width: '488px', textAlign: 'left' }}>Solusi Bagi Anda yang Memiliki Peternakan</h1>
-                        <p className='txt-poppins-medium' style={{ width: '395px', textAlign: 'justify', fontSize: '14px' }}>
+            <Container className='text-image' style={{ paddingLeft: '50px', paddingRight: '50px',paddingTop:'200px' }}>
+                <Row className='column-reverse'>
+                    <Col md={6} className='text-start'>
+                        <h1 className='txt-poppins-bold' 
+                        >Solusi Bagi Anda yang Memiliki Peternakan</h1>
+                        <p className='txt-poppins-medium'
+                         >
                             e-farm merupakan sebuah website yang membantu para peternak untuk mengelola peternakan dengan bantuan sistem.
                             e-farm memiliki fitur-fitur yang membantu untuk merawat hewan ternak,
                             seperti penjadwalan pemberian pakan dan vitamin kepada hewan ternak.
@@ -30,14 +32,17 @@ const Dashboard = () => {
                             <img src={`${process.env.PUBLIC_URL}Image-Assets/svg/arrow.svg`} alt='' style={{ marginLeft: '5px', width: '13px', height: '13px' }} />
                         </Button>
                     </Col>
-                    <Col>
-                        <Image src={`${process.env.PUBLIC_URL}Image-Assets/farmer.jpg`} style={{ height: '300px', width: '525px', borderRadius: '20px' }} />
+                    <Col md={6}>
+                        <Image className='img-dashboard' src={`${process.env.PUBLIC_URL}Image-Assets/farmer.jpg`} />
                     </Col>
                 </Row>
             </Container>
 
-            <Container style={{ width: '900px',marginTop:'200px' }}>
-                <h2 className='txt-poppins-semiBold' style={{ fontSize: '38.67px' }}>
+            <Container 
+            style={{ marginTop:'200px' }}
+            className='d-flex justify-content-center'
+            >
+                <h2 className='txt-poppins-semiBold' >
                     Hewan Ternak Apa Saja yang Dapat Dibantu Menggunakan E-Farm Saat Ini ?
                 </h2>
             </Container>
@@ -46,14 +51,19 @@ const Dashboard = () => {
                 <CustomCard /> {/* memanggil component card */}
             </Container>
 
-            <Container style={{ marginTop: '200px' }}>
+            <Container className='text-image tenaga-ahli' 
+            style={{paddingLeft: '50px', paddingRight: '50px',paddingTop:'200px' }}
+            >
                 <Row>
-                    <Col>
-                        <Image src={`${process.env.PUBLIC_URL}Image-Assets/tenaga-ahli.jpg`} style={{ height: '300px', width: '525px', borderRadius: '20px' }} />
+                    <Col md={6}>
+                        <Image className='img-dashboard' src={`${process.env.PUBLIC_URL}Image-Assets/tenaga-ahli.jpg`} 
+                        />
                     </Col>
-                    <Col className='text-start'>
-                        <h1 className='txt-poppins-bold' style={{ width: '488px', textAlign: 'left' }}>Kerjasama para ahli</h1>
-                        <p className='txt-poppins-medium' style={{ width: '395px', textAlign: 'justify', fontSize: '14px' }}>
+                    <Col md={6} className='text-start'>
+                        <h1 className='txt-poppins-bold'
+                        >Kerjasama para ahli</h1>
+                        <p className='txt-poppins-medium'
+                        >
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
                             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -66,12 +76,16 @@ const Dashboard = () => {
                 </Row>
             </Container>
 
-            <div className='d-flex align-items-center' style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/Image-Assets/ai-background.png)`, backgroundSize: '100% auto', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', height: '60vw' }}>
-                <Container style={{ paddingLeft: '100px', paddingRight: '100px' }}>
-                    <Row>
-                        <Col className='text-start'>
-                            <h1 className='txt-poppins-bold' style={{ width: '488px', textAlign: 'left' }}>Menggunakan Bantuan Artificial Intelegence</h1>
-                            <p className='txt-poppins-medium' style={{ width: '395px', textAlign: 'justify', fontSize: '14px' }}>
+            <div className='d-flex align-items-center' 
+            style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/Image-Assets/ai-background.png)`, backgroundSize: '100% auto', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', height: '60vw' }}
+            >
+                <Container className='bg-ai' style={{ paddingLeft: '100px', paddingRight: '100px' }}>
+                    <Row className='column-reverse'>
+                        <Col md={6} className='text-start'>
+                            <h1 className='txt-poppins-bold' 
+                            >Menggunakan Bantuan Artificial Intelegence</h1>
+                            <p className='txt-poppins-medium' 
+                            >
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
                                 dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -81,16 +95,17 @@ const Dashboard = () => {
                                 <img src={`${process.env.PUBLIC_URL}Image-Assets/svg/arrow-green.svg`} alt='' style={{ marginLeft: '5px', width: '13px', height: '13px' }} />
                             </Button>
                         </Col>
-                        <Col>
-                            <Image src={`${process.env.PUBLIC_URL}Image-Assets/ai-logo.png`} style={{ height: 'auto', width: 'auto', borderRadius: '20px' }} />
+                        <Col md={6}>
+                            <Image className='img-ai' src={`${process.env.PUBLIC_URL}Image-Assets/ai-logo.png`} 
+                            />
                         </Col>
                     </Row>
                 </Container>
             </div>
 
-            <Container style={{ border: '8px solid #DBAE84', borderRadius: '200px', width:'78%', marginBottom:'200px'}}>
+            <Container className='ads' style={{ border: '8px solid #DBAE84', borderRadius: '200px', width:'78%', marginBottom:'200px'}}>
                 <Row className='align-items-center'>
-                    <Col style={{paddingLeft:'120px'}}>
+                    <Col md={6} style={{paddingLeft:'120px'}}>
                         <h1 className='txt-poppins-bold' style={{ width: '488px', textAlign: 'left' }}>Siap Untuk Menjadi Bagian Dari Kami?</h1>
                         <div>
                             <Stack direction="horizontal" gap={2}>
@@ -101,8 +116,8 @@ const Dashboard = () => {
                     </Col>
 
 
-                    <Col className='p-0'>
-                        <Image src={`${process.env.PUBLIC_URL}Image-Assets/farmer2-removebg-preview.png`} style={{borderRadius: '20px', width:'auto'}} />
+                    <Col md={6} className='p-0'>
+                        <Image className='img-ai' src={`${process.env.PUBLIC_URL}Image-Assets/farmer2-removebg-preview.png`} style={{borderRadius: '20px', width:'auto'}} />
                     </Col>
                 </Row>
             </Container>
